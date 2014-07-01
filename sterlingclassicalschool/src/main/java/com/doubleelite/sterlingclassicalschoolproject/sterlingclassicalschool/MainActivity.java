c6dea611db7b2eb4a8ca1682b47466245ab35252
Max McKinney <maxgames101@gmail.com>
2014-07-01 18:46:32 -0500

    Working app now. Added in the date of the item as well.

--- sterlingclassicalschool/src/main/java/com/doubleelite/sterlingclassicalschoolproject/sterlingclassicalschool/MainActivity.java
+++ sterlingclassicalschool/src/main/java/com/doubleelite/sterlingclassicalschoolproject/sterlingclassicalschool/MainActivity.java
@@ -1,6 +1,7 @@
 package com.doubleelite.sterlingclassicalschoolproject.sterlingclassicalschool;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
@@ -22,6 +23,7 @@ import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Collections;
 
 
 public class MainActivity extends Activity {
@@ -30,18 +32,22 @@ public class MainActivity extends Activity {
     NewsItemAdapter newsItemAdapter;
     ArrayList<NewsItem> newsItems;
     NewsItem item;
-    InputStream urlInputStream;
+
+    Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Find android views
         lvNewsItems = (ListView)findViewById(R.id.lv_newsItems);
 
         // Initializing instance variables
         newsItems = new ArrayList<NewsItem>();
+        context = this;
 
+        // Download the latest feed
         try {
             new URLAsyncTask().execute(new URL("http://feeds.feedburner.com/SterlingClassicalSchool?format=xml"));
         } catch (MalformedURLException e) {
@@ -69,14 +75,17 @@ public class MainActivity extends Activity {
         return super.onOptionsItemSelected(item);
     }
 
-    public void ParseXMLAndShowIt(InputStream stream) {
-        try {
-            XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
-            factory.setNamespaceAware(false);
-            XmlPullParser xpp = factory.newPullParser();
+    private class URLAsyncTask extends AsyncTask<URL, String, ArrayList<NewsItem>> {
+        // Use the URL passed in the AysncClass and return an InputStream to be used in onPostExecute
+        @Override
+        protected ArrayList<NewsItem> doInBackground(URL... params) {
+            try {
+                XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
+                factory.setNamespaceAware(false);
+                XmlPullParser xpp = factory.newPullParser();
 
-            // We will get the XML from an input stream
-            xpp.setInput(stream, "UTF_8");
+                // We will get the XML from an input stream
+                xpp.setInput(params[0].openConnection().getInputStream(), "UTF_8");
 
             /* We will parse the XML content looking for the "<title>" tag which appears inside the "<item>" tag.
              * However, we should take in consideration that the rss feed name also is enclosed in a "<title>" tag.
@@ -86,60 +95,53 @@ public class MainActivity extends Activity {
              *
              * In order to achieve this, we will make use of a boolean variable.
              */
-            boolean insideItem = false;
+                boolean insideItem = false;
 
-            // Returns the type of current event: START_TAG, END_TAG, etc..
-            int eventType = xpp.getEventType();
-            while (eventType != XmlPullParser.END_DOCUMENT) {
-                if (eventType == XmlPullParser.START_TAG) {
+                // Returns the type of current event: START_TAG, END_TAG, etc..
+                int eventType = xpp.getEventType();
+                while (eventType != XmlPullParser.END_DOCUMENT) {
+                    if (eventType == XmlPullParser.START_TAG) {
 
-                    if (xpp.getName().equalsIgnoreCase("item")) {
-                        insideItem = true;
-                        item = new NewsItem();
-                    } else if (xpp.getName().equalsIgnoreCase("title")) {
-                        if (insideItem)
-                            item.title = xpp.nextText();
-                    } else if (xpp.getName().equalsIgnoreCase("link")) {
-                        if (insideItem) {}
-                        //links.add(xpp.nextText()); //extract the link of article
+                        if (xpp.getName().equalsIgnoreCase("item")) {
+                            insideItem = true;
+                            item = new NewsItem();
+                        } else if (xpp.getName().equalsIgnoreCase("title")) {
+                            if (insideItem)
+                                item.title = xpp.nextText();
+                        } else if (xpp.getName().equalsIgnoreCase("description")) {
+                            if (insideItem){}
+                                //item.description = xpp.nextText();
+                            //links.add(xpp.nextText()); //extract the link of article
+                        } else if (xpp.getName().equalsIgnoreCase("pubDate")) {
+                            if (insideItem)
+                                item.date = xpp.nextText();
+                        }
+
+                    } else if(eventType==XmlPullParser.END_TAG && xpp.getName().equalsIgnoreCase("item")){
+                        insideItem=false;
+                        newsItems.add(item);
                     }
-                } else if(eventType==XmlPullParser.END_TAG && xpp.getName().equalsIgnoreCase("item")){
-                    insideItem=false;
-                    newsItems.add(item);
+                    eventType = xpp.next(); //move to next element
                 }
-                eventType = xpp.next(); //move to next element
-            }
 
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        } catch (XmlPullParserException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        newsItemAdapter = new NewsItemAdapter(this, R.layout.news_item, newsItems);
-
-        lvNewsItems.setAdapter(newsItemAdapter);
-
-    }
-
-    private class URLAsyncTask extends AsyncTask<URL, String, InputStream> {
-        // Use the URL passed in the AysncClass and return an InputStream to be used in onPostExecute
-        @Override
-        protected InputStream doInBackground(URL... params) {
-            try {
-                Log.v("APP", "Downloading File");
-                return params[0].openConnection().getInputStream();
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (XmlPullParserException e) {
+                e.printStackTrace();
             } catch (IOException e) {
-                return null;
+                e.printStackTrace();
             }
+            // This is the only way I know of showing the most recently added items in the calender at the top
+            // If this wasn't here the newest items would be at the bottom
+            Collections.reverse(newsItems);
+            return newsItems;
         }
 
         @Override
-        protected void onPostExecute(InputStream inputStream) {
+        protected void onPostExecute(ArrayList<NewsItem> newsItem) {
             Log.v("APP", "Done downloading now parse it");
-            ParseXMLAndShowIt(inputStream);
+            newsItemAdapter = new NewsItemAdapter(context, R.layout.news_item, newsItem);
+            lvNewsItems.setAdapter(newsItemAdapter);
         }
     }
 
