9b8bf25f762a4cbfeb04c2049a006c323f411ac9
Max McKinney <maxgames101@gmail.com>
2014-07-29 10:32:06 -0500

    Fixed the issue with Parse and multiple notifications if the user reinstalled the app. Also fixed the issue with the app showing a black screen (used saveEventually instead of saveInBackground for the installing object)

--- sterlingclassicalschool/src/main/java/com/doubleelite/sterlingclassicalschoolproject/sterlingclassicalschool/MainActivity.java
+++ sterlingclassicalschool/src/main/java/com/doubleelite/sterlingclassicalschoolproject/sterlingclassicalschool/MainActivity.java
@@ -10,6 +10,7 @@ import android.content.SharedPreferences;
 import android.content.res.Configuration;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.v4.app.ActionBarDrawerToggle;
 import android.support.v4.widget.DrawerLayout;
 import android.util.Log;
--- sterlingclassicalschool/src/main/java/com/doubleelite/sterlingclassicalschoolproject/sterlingclassicalschool/MainApplication.java
+++ sterlingclassicalschool/src/main/java/com/doubleelite/sterlingclassicalschoolproject/sterlingclassicalschool/MainApplication.java
@@ -1,6 +1,7 @@
 package com.doubleelite.sterlingclassicalschoolproject.sterlingclassicalschool;
 
 import android.app.Application;
+import android.provider.Settings;
 import android.util.Log;
 
 import com.parse.Parse;
@@ -18,8 +19,23 @@ public class MainApplication extends Application {
         Log.v("APP", "Application - Initialize Parse");
         // Setup Parse for push notifications
         Parse.initialize(this, "dbgMLg4QssgeHn6ogl9qcorJ74IYWBO29d731nqn", "QUNUh1yKcSBfIko45FDOVvPid7oakpmZzllNsnkF");
-        // Set the icon in the status bar
+
+        // Set the icon in the status bar and the activity to launch when you tap the notification.
         PushService.setDefaultPushCallback(this, MainActivity.class, R.drawable.ic_notification_sterling);
-        ParseInstallation.getCurrentInstallation().saveInBackground();
+
+        // Get the Android_ID of this device
+        String  android_id = Settings.Secure.getString(getApplicationContext().getContentResolver(), Settings.Secure.ANDROID_ID);
+        Log.e("LOG","android id >>" + android_id);
+
+        // Add the id to the installation, this should help prevent multiple notifications if the user uninstalls
+        // and reinstalls the app.
+        ParseInstallation installation = ParseInstallation.getCurrentInstallation();
+        installation.put("UniqueId",android_id);
+
+        // If the user doesn’t have a network connection, the object will be stored
+        // safely on the device until a new connection has been established.
+        // If the app is closed before the connection is back, Parse will try to
+        // save it again the next time the app is opened.
+        installation.saveEventually();
     }
 }
